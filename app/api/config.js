50d7077204790abef1f0810368708a8269489eef
S3gaM3 <kiborgik87@gmail.com>
2024-05-26 22:21:05 +0300

    ф

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://denizho.nomoredomainswork.ru/api";
+export const BASE_URL = "https://s3ga.nomoredomainswork.ru/api";
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
